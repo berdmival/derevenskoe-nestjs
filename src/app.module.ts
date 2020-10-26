42bfdfd3bd32ab2e2c4a4e4863a5cb11c6b36620
berdmival <mtsby7049874@gmail.com>
2020-10-26 21:38:49 +0300

    project basis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true })],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot({ autoSchemaFile: true }),
+    ProductModule,
+    CategoryModule,
+    UserModule,
+    AuthModule,
+    OrderModule,
+  ],
   controllers: [],
   providers: [],
 })
