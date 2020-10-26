86ad25cf1028d16010c3ee2f46ca63eed4c0a6b9
berdmival <mtsby7049874@gmail.com>
2020-10-26 22:15:50 +0300

    project base p2

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { CategoryResolver } from './category.resolver';
 
 @Module({
-  providers: [CategoryService]
+  providers: [CategoryService, CategoryResolver]
 })
 export class CategoryModule {}
