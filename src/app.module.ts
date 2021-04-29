b72302abb24cf51be64b75a45344e3cbc4339eab
berdmival <mtsby7049874@gmail.com>
2021-04-30 02:19:59 +0300

    setup docker compose for local development

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { UserModule } from './user/user.module';
@@ -10,16 +10,19 @@ import { OrderModule } from './order/order.module';
 import { ImageModule } from './image/image.module';
 import configuration from './configuration/default';
 
+const dbConfiguration = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+  synchronize: true, // TODO: remove this in production
+} as TypeOrmModuleOptions;
+
+if (!process.env.LOCAL) dbConfiguration['ssl'] = { rejectUnauthorized: false };
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-      entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
-      synchronize: true, // TODO: remove this in production
-    }),
+    TypeOrmModule.forRoot({ ...dbConfiguration }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       context: ({ req, res, ...ctx }) => ({ req, res, ...ctx }),
